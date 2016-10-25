a515aca9152ea7304a8ce238cd4213b25c9437a3
pooler22 <rudypawel12@gmail.com>
2016-10-25 23:34:47 +0200

    clean up code

--- TGK/main.cpp
+++ TGK/main.cpp
@@ -14,10 +14,10 @@ public:
 	float *a, *b, *c, *color;
 };
 
-Triangle::Triangle(vector<float>abc, float*color1)
+Triangle::Triangle(vector<float> abc, float* color1)
 {
 	this->a = new float[2]{ abc[0], abc[1] };
-	this->b = new float[2]{ abc[0]+ abc[2], abc[1] };
+	this->b = new float[2]{ abc[0] + abc[2], abc[1] };
 	this->c = new float[2]{ abc[0], abc[1] + abc[2] };
 	this->color = new float[4]{ color1[0], color1[1],color1[2], color1[3] };
 }
@@ -33,18 +33,19 @@ void Triangle::Draw() const
 }
 
 
-void MyDisplay(void) {
+void MyDisplay(void)
+{
 	// Wyswietlana scena - poczatek
 	glLoadIdentity();
 	glClear(GL_COLOR_BUFFER_BIT);
 
 	float colors[6][4] = {
-		{ 1,0,0,1 },
-		{ 1,1,0,1 },
-		{ 1,1,1,1 },
-		{ 1,0,1,1 },
-		{ 0,1,0,1 },
-		{ 0,0,1,1 }
+		{1,0,0,1},
+		{1,1,0,1},
+		{1,1,1,1},
+		{1,0,1,1},
+		{0,1,0,1},
+		{0,0,1,1}
 	};
 
 	vector<vector<float>> points;
@@ -57,7 +58,7 @@ void MyDisplay(void) {
 	{
 		for (auto k = 0; k < j; k++)
 		{
-			points.push_back({ sx + (a*i),sy + (a*k), a});
+			points.push_back({ sx + (a * i),sy + (a * k), a });
 		}
 	}
 
@@ -68,17 +69,20 @@ void MyDisplay(void) {
 
 	glFlush();//start processing buffered OpenGL routines
 }
-void MyInit(void) {
+
+void MyInit(void)
+{
 	glClearColor(0.0, 0.0, 0.0, 0.0);//select clearing (background) color
 	glViewport(0, 0, 300, 300);//pocz•tek u.ws. lewy górny róg
 	glMatrixMode(GL_PROJECTION);//Nast•pne 2 wiersze b•d• modyfikowa³y m. PROJECTION
 	glLoadIdentity();//inicjalizacja
-	gluOrtho2D(0.0, 500.0*1.2, 0.0, 500.0);
+	gluOrtho2D(0.0, 500.0 * 1.2, 0.0, 500.0);
 	glMatrixMode(GL_MODELVIEW); //Nast•pny wiersz b•dzie modyfikowa³ m. MODELVIEW
 	glLoadIdentity();
 }
 
-int main(int argc, char** argv) {
+int main(int argc, char** argv)
+{
 	glutInit(&argc, argv);
 	glutInitDisplayMode(GLUT_SINGLE | GLUT_RGB);//single buffer and RGBA
 	glutInitWindowSize(250, 250);//initial window size
@@ -89,5 +93,3 @@ int main(int argc, char** argv) {
 	glutMainLoop();//enter main loop and process events
 	return 0;
 }
-
-
